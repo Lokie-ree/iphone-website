b022f7de3d83d7701e105e3a751174c77afcaa1d
Lokie-ree <165349722+Lokie-ree@users.noreply.github.com>
2024-07-23 22:26:22 -0500

    added Highlight components and began working on Video Carousel component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,11 +3,12 @@ import { appleImg, bagImg, searchImg } from '../utils';
 import { navLists } from '../constants';
 
 const Navbar = () => {
+
   return (
     <>
       <header className="flex justify-between items-center w-full py-5 sm:px-10 px-5">
         <nav className="flex w-full screen-max-width">
-          <img src={appleImg} alt="Apple" width={14} height={18}/>
+          <img id="apple-img" src={appleImg} alt="Apple" width={14} height={18}/>
 
           <div className="flex flex-1 justify-center max-sm:hidden">
             {navLists.map((nav) => (
