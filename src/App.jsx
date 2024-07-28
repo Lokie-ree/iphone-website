899f2d378c84350c2f724b31e8d64172654a0d05
Lokie-ree <165349722+Lokie-ree@users.noreply.github.com>
2024-07-27 19:21:33 -0500

    began working on Model and ModelView components

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Highlights from './components/Highlights'
+import Model from './components/Model'
 
 const App = () => {
 
@@ -10,6 +11,7 @@ const App = () => {
       <Navbar />
       <Hero />
       <Highlights />
+      <Model />
     </main>
   )
 }
