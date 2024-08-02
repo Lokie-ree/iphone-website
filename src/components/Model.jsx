4821b2387f3711030f8e0087f1f27744bb7e1a58
Lokie-ree <165349722+Lokie-ree@users.noreply.github.com>
2024-08-02 10:21:16 -0500

    finished iPhone website by adding final animations to 3D models in the Model/ModelView components

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -1,6 +1,6 @@
 import { useGSAP } from '@gsap/react'
 import gsap from 'gsap'
-import React, {useState, useRef} from 'react'
+import React, {useState, useRef, useEffect} from 'react'
 import ModelView from './ModelView'
 import { yellowImg } from '../utils'
 
@@ -8,6 +8,7 @@ import * as THREE from 'three';
 import { Canvas } from '@react-three/fiber'
 import { View } from '@react-three/drei'
 import { models, sizes } from '../constants'
+import { animateWithGSAPTimeLine } from '../utils/animations'
 
 const Model = () => {
 
@@ -28,6 +29,24 @@ const Model = () => {
 
   const [smallRotation, setSmallRotation] = useState(0);
   const [largeRotation, setLargeRotation] = useState(0);
+
+  const tl = gsap.timeline();
+
+  useEffect(() => {
+    if(size === 'large') {
+      animateWithGSAPTimeLine(tl, small, smallRotation, '#view1', '#view2', {
+        transform: 'translateX(-100%)',
+        duration: 2
+      })
+    }
+
+    if(size === 'small') {
+      animateWithGSAPTimeLine(tl, large, largeRotation, '#view2', '#view1', {
+        transform: 'translateX(0)',
+        duration: 2
+      })
+    }
+  }, [size])
 
   useGSAP(() => {
     gsap.to('#heading', { y: 0, opacity: 1 })
@@ -53,7 +72,7 @@ const Model = () => {
             />
 
             <ModelView
-              index={1}
+              index={2}
               groupRef={large}
               gsapType='view2'
               controlRef={cameraControlLarge}
--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,8 +1,8 @@
-import { OrbitControls, PerspectiveCamera, View } from '@react-three/drei'
+import { Html, OrbitControls, PerspectiveCamera, View } from '@react-three/drei'
 import React, { Suspense } from 'react'
 import Lights from './Lights'
 import IPhone from './IPhone'
-import { Html } from '@react-three/drei'
+import Loader from './Loader'
 import * as THREE from 'three'
 
 const ModelView = ({ index, groupRef, gsapType, controlRef, setRotationState, size, item }) => {
@@ -10,7 +10,7 @@ const ModelView = ({ index, groupRef, gsapType, controlRef, setRotationState, si
     <View
       index={index}
       id={gsapType}
-      className={`w-full h-full ${index === 2} ? 'right-[-100%] : ''`}
+      className={`w-full h-full absolute ${index === 2 ? 'right-[-100%]' : ''}`}
     >
       {/* Ambient Light */}
       <ambientLight intensity={0.3} />
@@ -29,9 +29,8 @@ const ModelView = ({ index, groupRef, gsapType, controlRef, setRotationState, si
         onEnd={() => setRotationState(controlRef.current.getAzimuthalAngle())}
       />
 
-      <group ref={groupRef} name={`${index === 1} ?
-      'small' : 'large`} position={[0, 0, 0]}>
-        <Suspense fallback={<Html><div>Loading</div></Html>}>
+      <group ref={groupRef} name={`${index === 1} ? 'small' : 'large`} position={[0, 0, 0]}>
+        <Suspense fallback={<Loader />}>
           <IPhone
             scale={index === 1 ? [15, 15, 15]: [17, 17, 17]}
             item={item}
@@ -39,8 +38,6 @@ const ModelView = ({ index, groupRef, gsapType, controlRef, setRotationState, si
           />
         </Suspense>
       </group>
-     
-
     </View>
   )
 }
