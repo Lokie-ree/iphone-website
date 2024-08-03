e6a8505333af2199840cd36ea0ba419a12d719f3
Lokie-ree <165349722+Lokie-ree@users.noreply.github.com>
2024-08-03 16:57:04 -0500

    Features section completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
-import React from 'react';
-import Navbar from './components/Navbar'
-import Hero from './components/Hero'
-import Highlights from './components/Highlights'
-import Model from './components/Model'
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Highlights from './components/Highlights';
+import Model from './components/Model';
+import Features from './components/Features';
+
 
 const App = () => {
-
   return (
     <main className="bg-black">
       <Navbar />
       <Hero />
       <Highlights />
       <Model />
+      <Features />
     </main>
   )
 }
 
-export default App
+export default App;
